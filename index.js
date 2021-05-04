2ed628754a627d424d71c541a2c8dd7658f54885
jonhayjr <jonhayjr@gmail.com>
2021-05-03 18:37:03 -0700

    Made some changes to express.js project template

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
+//Require express
 const express = require('express');
+
+//Express App
 const app = express();
 
+//Configure pug
 app.set('view engine', 'pug');
 
+//Set path for static files
 app.use('/static', express.static('public'));
 
+//Get index route
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+//Get about route
 app.get('/about', (req, res, next) => {
   res.render('about');
 });
 
+//Get error route
 app.get('/error', (req, res, next) => {
   const err = new Error();
   err.status = 500;
   err.message = 'Oops, something went wrong!';
-  //res.render('error', {err});
-  next({err});
+  next(err);
 })
 
+//Handle 404 error
 app.use((req,res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   err.message = 'Page Not Found';
-  //res.render('404', {err});
-  next({err});
+  res.render('404', {err});
 });
 
+//Global error handler
 app.use((err, req, res, next) => {
-  res.locals.error = err;
   err.status = err.status || 500;
   err.message = err.message || 'Oops, something went wrong!';
   
@@ -44,7 +51,7 @@ app.use((err, req, res, next) => {
 
 });
 
-
+//Listen on port 3001
 app.listen(3001, () => {
   console.log('Server listening on port 3001.')
 });
